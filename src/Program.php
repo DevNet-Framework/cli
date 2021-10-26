6430fca25c02ccbbd81f5996b7348127d96ddb5f
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-10-26 01:47:44 -0400

    update Program

--- src/Program.php
+++ src/Program.php
@@ -13,6 +13,9 @@ use DevNet\Cli\Commands\AddCommand;
 use DevNet\Cli\Commands\MigrateCommand;
 use DevNet\Cli\Commands\NewCommand;
 use DevNet\Cli\Commands\RunCommand;
+use DevNet\System\Command\CommandEventArgs;
+use DevNet\System\Command\CommandLine;
+use DevNet\System\Command\CommandOption;
 use DevNet\System\IO\ConsoleColor;
 use DevNet\System\IO\Console;
 
@@ -20,73 +23,40 @@ class Program
 {
     public static function main(array $args = [])
     {
-        $dispatcher = new CommandDispatcher();
+        $rootCommand = new CommandLine();
+        $rootCommand->addCommand(new AddCommand);
+        $rootCommand->addCommand(new NewCommand);
+        $rootCommand->addCommand(new RunCommand);
+        $rootCommand->addCommand(new MigrateCommand);
+        $rootCommand->addOption(new CommandOption('--help', '-h'));
+        $rootCommand->addOption(new CommandOption('--version', '-v'));
+        $rootCommand->Handler->add(new self, 'execute');
+        $result = $rootCommand->invoke($args);
 
-        $dispatcher->addCommand(function (CommandLine $command) {
-            $command->setName('add');
-            $command->setDescription('Add a template code file to the project.');
-            $command->addArgument('template');
-            $command->addOption('--name');
-            $command->addOption('--directory');
-            $command->addOption('--help');
-            $command->OnExecute(new AddCommand(), 'execute');
-        });
-
-        $dispatcher->addCommand(function (CommandLine $command) {
-            $command->setName('new');
-            $command->setDescription('Create a new project.');
-            $command->addArgument('template');
-            $command->addOption('--project');
-            $command->addOption('--help');
-            $command->OnExecute(new NewCommand(), 'execute');
-        });
-
-        $dispatcher->addCommand(function (CommandLine $command) {
-            $command->setName('migrate');
-            $command->setDescription('Migrate database schema and data.');
-            $command->addOption('--target');
-            $command->addOption('--help');
-            $command->OnExecute(new MigrateCommand(), 'execute');
-        });
-
-        $dispatcher->addCommand(function (CommandLine $command) {
-            $command->setName('run');
-            $command->setDescription('Run the DevNet applicaton.');
-            $command->addOption('--project');
-            $command->addOption('--help');
-            $command->OnExecute(new RunCommand(), 'execute');
-        });
-
-        self::processArgs($dispatcher, $args);
-    }
-
-    public static function processArgs(CommandDispatcher $dispatcher, array $args): void
-    {
-        $argument = $args[0] ?? null;
-
-        if ($argument == '--path') {
-            self::showPath();
+        if (!$result) {
+            Console::foregroundColor(ConsoleColor::Red);
+            Console::writeline("The specified command or argument was not found, try 'devnet --help' for more infromation.");
+            Console::resetColor();
         }
 
-        if ($argument == '--version') {
+        exit;
+    }
+
+    public function execute(object $sender, CommandEventArgs $args): void
+    {
+        $help = $args->get('--help');
+        if ($help) {
+            self::showHelp($sender);
+            return;
+        }
+
+        $version = $args->get('--version');
+        if ($version) {
             self::showVersion();
         }
-
-        if ($argument == '--help') {
-            self::showHelp($dispatcher);
-        }
-
-        $result = $dispatcher->invoke($args);
-
-        if (!$result || !$argument) {
-            Console::foregroundColor(ConsoleColor::Red);
-            Console::writeline("The specified command was not found, try 'devnet --help' for more help.");
-            Console::resetColor();
-            exit;
-        }
     }
 
-    public static function showHelp(CommandDispatcher $dispatcher): void
+    public static function showHelp(object $sender): void
     {
         Console::writeline("DevNet command-line interface v1.0.0");
         Console::writeline("Usage: devnet [options]");
@@ -94,43 +64,34 @@ class Program
         Console::writeline("Options:");
         Console::writeline("  --help      Show command line help.");
         Console::writeline("  --version   Show DevNet Cli version.");
-        Console::writeline("  --path      Show DevNet runtime path.");
         Console::writeline();
         Console::writeline("Usage: devnet [command] [arguments] [options]");
         Console::writeline();
         Console::writeline("commands:");
         $super = 0;
-        $commands = $dispatcher->Commands;
+        $commands = $sender->Commands;
 
         foreach ($commands as $command) {
-            $lenth = strlen($command->getName());
+            $lenth = strlen($command->Name);
             if ($lenth > $super) {
                 $super = $lenth;
             }
         }
 
         foreach ($commands as $command) {
-            $lenth = strlen($command->getName());
+            $lenth = strlen($command->Name);
             $steps = $super - $lenth + 3;
             $space = str_repeat(" ", $steps);
-            Console::writeline("  {$command->getName()}$space{$command->getDescription()}");
+            Console::writeline("  {$command->Name}$space{$command->Description}");
         }
 
         Console::writeline();
         Console::writeline("Run 'devnet [command] --help' for more information on a command.");
-        exit;
     }
 
     public static function showVersion(): void
     {
         Console::writeline("DevNet command-line interface v1.0.0");
         Console::writeline("Copyright (c) DevNet");
-        exit;
-    }
-
-    public static function showPath(): void
-    {
-        Console::writeline(dirname(__DIR__, 2));
-        exit;
     }
 }
