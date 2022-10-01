05907aca64d5927a2196bb6335536eb03be15bc7
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-10-01 00:43:05 -0400

    add ITemplateProvider

---
+++ src/Templating/ITemplateProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\Cli\Templating;
+
+interface ITemplateProvider
+{
+    /**
+     * Get the template name
+     */
+    public function getName(): string;
+
+    /**
+     * Get the template description
+     */
+    public function getDescription(): string;
+
+    /**
+     * Get the template source path
+     */
+    public function getSourcePath(): string;
+}
