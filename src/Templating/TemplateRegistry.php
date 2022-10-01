2c3fe2d9c101167e37c1a02eca99bcd858534a8d
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-10-01 00:45:24 -0400

    add TemplateRegistry

---
+++ src/Templating/TemplateRegistry.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\Cli\Templating;
+
+use DevNet\Cli\Commands\AbstractRegistry;
+
+class TemplateRegistry extends AbstractRegistry
+{
+    private static ?TemplateRegistry $instance = null;
+
+    public static function getSingleton(): static
+    {
+        if (!static::$instance) {
+            static::$instance = new TemplateRegistry(ITemplateProvider::class);
+        }
+
+        return static::$instance;
+    }
+}
