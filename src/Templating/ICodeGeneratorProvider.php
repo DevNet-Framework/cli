8ac14b12a5d15b00d655475e7c46052ab269c28f
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-10-04 00:40:27 -0400

    add ICodeGeneratorProvider

---
+++ src/Templating/ICodeGeneratorProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\Cli\Templating;
+
+interface ICodeGeneratorProvider
+{
+    /**
+     * Get the generator name
+     */
+    public function getName(): string;
+
+    /**
+     * Get the generator description
+     */
+    public function getDescription(): string;
+
+    /**
+     * Get the code generator
+     */
+    public function getGenerator(): ICodeGenerator;
+}
