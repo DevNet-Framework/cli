3f81126deb1807693bb1f70242d2793a5a57c981
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2022-10-05 00:44:20 -0400

    add CodeGeneratorRegistry

---
+++ src/Templating/CodeGeneratorRegistry.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @author      Mohammed Moussaoui
+ * @copyright   Copyright (c) Mohammed Moussaoui. All rights reserved.
+ * @license     MIT License. For full license information see LICENSE file in the project root.
+ * @link        https://github.com/DevNet-Framework
+ */
+
+namespace DevNet\Cli\Templating;
+
+use DevNet\Cli\Commands\AbstractRegistry;
+
+class CodeGeneratorRegistry extends AbstractRegistry
+{
+    private static ?CodeGeneratorRegistry $instance = null;
+
+    public static function getSingleton(): static
+    {
+        if (!static::$instance) {
+            static::$instance = new CodeGeneratorRegistry(ICodeGeneratorProvider::class);
+        }
+
+        return static::$instance;
+    }
+}
